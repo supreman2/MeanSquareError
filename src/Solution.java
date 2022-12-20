68e60c08b4fbfbe2100ce3ef0d4125c42283a17f
supreman <supreman2@gmail.com>
2022-12-20 23:41:11 +0300

    https://www.codewars.com/kata/51edd51599a189fe7f000015/train/java

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,16 @@
 public class Solution {
     public static double solution(int[] arr1, int[] arr2) {
-        return 0;
+
+        int sq = 0;
+
+        for (int i = 0; i < arr1.length; i++) {
+
+            int diff=arr1[i]-arr2[i];
+            sq = sq + diff*diff;
+
+        }
+
+        return  (double) sq/arr1.length;
+
     }
 }
